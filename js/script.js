63011b2cef97a50dd0e20949e340b41395af5ca6
ROOTPOWN <rootpown@gmail.com>
2024-03-07 20:36:18 +0400

    изменение структуры кода с терминала в браузерной

---
+++ js/script.js
@@ -0,0 +1,67 @@
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+let playerResult = 0;
+let computerResult = 0;
+let gameResult = "";
+function gameStart(playerSelection, computerSelection) {
+  // if (!["ROCK", "PAPER", "SCISSORS"].includes(playerSelection)) {
+  //   throw new Error("Invalid input. Please enter ROCK, PAPER, or SCISSORS.");
+  // }
+  if (playerSelection === computerSelection) {
+    gameResult = "draw";
+  } else if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+  ) {
+    playerResult++;
+    gameResult = "You Won!";
+  } else {
+    computerResult++;
+    gameResult = "You Lost!";
+  }
+  document.getElementById(
+    "game-result"
+  ).innerText = `Player Result: ${playerResult}, Computer Result ${computerResult}, Game Result: ${gameResult}`;
+}
+
+document
+  .getElementById("ROCK")
+  .addEventListener("click", () => gameStart("ROCK"));
+document
+  .getElementById("PAPER")
+  .addEventListener("click", () => gameStart("PAPER"));
+document
+  .getElementById("SCISSORS")
+  .addEventListener("click", () => gameStart("SCISSORS"));
+// function printResult() {
+//   console.log(`Player result: ${playerResult}`);
+//   console.log(`Computer result: ${computerResult}`);
+//   console.log(`Game result: ${gameResult}`);
+// }
+// function playGame() {
+//   rl.question("Enter your choice (ROCK, PAPER, or SCISSORS): ", (input) => {
+//     try {
+//       const playerSelection = input.toUpperCase();
+//       const computerOptions = ["ROCK", "PAPER", "SCISSORS"];
+//       const computerSelection =
+//         computerOptions[Math.floor(Math.random() * computerOptions.length)];
+//       gameStart(playerSelection, computerSelection);
+
+//       if (playerResult === 3 || computerResult === 3) {
+//         console.log("Game Result: ", gameResult);
+//         rl.close();
+//       } else {
+//         printResult();
+//         playGame();
+//       }
+//     } catch (error) {
+//       console.error(error.message);
+//       playGame();
+//     }
+//   });
+// }
+playGame();
