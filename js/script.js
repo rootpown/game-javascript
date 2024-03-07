78a62f4632f0ca84f848fd767656c08f1b035190
ROOTPOWN <rootpown@gmail.com>
2024-03-08 00:16:18 +0400

    изменил логику игры

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-
 let playerResult = 0;
 let computerResult = 0;
 let gameResult = "";
 function gameStart(playerSelection, computerSelection) {
-
+  const ROCK = document.getElementById("ROCK");
+  const PAPER = document.getElementById("PAPER");
+  const SCISSORS = document.getElementById("SCISSORS");
+  ROCK.addEventListener("click", () => {
+    playRound("ROCK");
+  });
+  PAPER.addEventListener("click", () => {
+    playRound("PAPER");
+  });
+  SCISSORS.addEventListener("click", () => {
+    playRound("SCISSORS");
+  });
+}
+function playRound(playerSelection) {
+  const computerSelection = computerPlay();
   if (playerSelection === computerSelection) {
     gameResult = "draw";
   } else if (
@@ -17,8 +30,25 @@ function gameStart(playerSelection, computerSelection) {
     computerResult++;
     gameResult = "You Lost!";
   }
-  document.getElementById(
-    "game-result"
-  ).innerText = `Player Result: ${playerResult}, Computer Result ${computerResult}, Game Result: ${gameResult}`;
+
+  gameResultation();
 }
-);
+function computerPlay() {
+  const computerChoises = ["ROCK", "PAPER", "SCISSORS"];
+  return computerChoises[Math.floor(Math.random() * computerChoises.length)];
+}
+function gameResultation() {
+  const resultElement = document.getElementById("game-result");
+  resultElement.textContent = `Player: ${playerResult} Computer ${computerResult} `;
+  if (playerResult === 3 || computerResult === 3) {
+    if (playerResult === 3) {
+      resultElement.textContent = "You Won ##PLAYER";
+    } else {
+      resultElement.textContent = "Computer WON ##YOU LOST";
+    }
+    playerResult = 0;
+    computerResult = 0;
+  }
+}
+
+gameStart();
